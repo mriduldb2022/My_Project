ce93ea1a433729ab63eeece85501c9b2d638a008
mridulmac2019 <mridulmac2019@localhost>
2023-06-08 12:41:18 -0400

    Actions and Loop

--- My_Project/src/test/java/com/Practice/ReverseString.java
+++ My_Project/src/test/java/com/Practice/ReverseString.java
@@ -8,20 +8,40 @@ public class ReverseString {
 		//using StringBuffer 
 		
 //		String s = new String("My name is Mridul");
+//		System.out.println(s);
 //		String s2 = new StringBuffer(s).reverse().toString();
 //		System.out.println(s2);
 		
+		// practice:
+		String g = new String("God");
+		System.out.println(g);
+		String g2 = new StringBuffer(g).reverse().toString();
+		System.out.println(g2);
+		
+		
+		
 		
 		//using ForLoop
-		String str = "My name is Mridul";
-		String rev = "";
 		
-		for (int i=str.length()-1; i>=0; i--) {
-			rev = rev+str.charAt(i);
+//		String str = "My name is Mridul";
+//		String rev = "";
+//		
+//		for (int i=str.length()-1; i>=0; i--) {
+//			rev = rev+str.charAt(i);
+//		}
+//		System.out.println(rev);
+		
+		
+		String r = "fool";
+		String revR = "";
+		for(int i=r.length()-1; i>=0; i--) {
+			revR = revR+r.charAt(i);
 		}
-		System.out.println(rev);
+		System.out.println(revR);
 	}
 	
+		
+		
 	
+	}
 
-}
